7245da46bec222dc77c8a7c798c9b2b711000d91
InsaneDev2478 <vedansh.shetti@gmail.com>
2026-01-30 19:08:54 +0000

    updated layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Separator from "./components/separator";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <span className="text-center font-semibold font-mono bg-slate-100 dark:bg-slate-800 p-1.5 rounded-md">@vedanshshetti/</span>
         <span className="font-bold">SimplyUI</span>
       </h1>
-      <div className="bg-slate-200 dark:bg-slate-900 h-1.5 rounded-md w-[90%]"></div>
+      <Separator length="75"/>
       <Link href="/docs">Click to get started!</Link>
     </div>
   );
